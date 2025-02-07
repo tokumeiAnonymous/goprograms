afac99b17d8018ea4a45679031e54a622e60f01a
Jerome Taguba <jerometagubanoone@gmail.com>
2025-02-07 13:00:42 -0800

    Added temp file functionality

--- markup/go.mod
+++ markup/go.mod
@@ -5,7 +5,7 @@ go 1.21.0
 require (
 	github.com/aymerick/douceur v0.2.0 // indirect
 	github.com/gorilla/css v1.0.1 // indirect
-	github.com/microcosm-cc/bluemonday v1.0.27 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/microcosm-cc/bluemonday v1.0.27
+	github.com/russross/blackfriday/v2 v2.1.0
 	golang.org/x/net v0.26.0 // indirect
 )
--- markup/main.go
+++ markup/main.go
@@ -4,8 +4,8 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"io"
 	"os"
-	"path/filepath"
 
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday/v2"
@@ -25,15 +25,20 @@ const (
 	`
 )
 
-func run(filename string) error {
+func run(filename string, out io.Writer) error {
 	// Read all the data from the input file and check for errors
 	input, err := os.ReadFile(filename)
 	if err != nil {
 		return err
 	}
+	temp, err := os.CreateTemp("", "mdp*.html")
+	if err != nil {
+		fmt.Println("Error creating temp file:", err)
+		return err
+	}
 	htmlData := parseContent(input)
-	outName := fmt.Sprintf("%s.html", filepath.Base(filename))
-	fmt.Println(outName)
+	outName := temp.Name()
+	fmt.Fprintln(out, outName)
 	return saveHTML(outName, htmlData)
 }
 
@@ -64,7 +69,7 @@ func main() {
 		flag.Usage()
 		os.Exit(1)
 	}
-	if err := run(*filename); err != nil {
+	if err := run(*filename, os.Stdout); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
